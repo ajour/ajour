3e86dad5aa1806003b21754d915f94f5a2660082
Casper Storm <casper+github@rogildstorm.com>
2020-07-03 11:54:37 +0200

    chore: refactored into gui/

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
+mod gui;
 mod toc;
-mod window;
 
 use crate::toc::read_addon_dir;
-use crate::window::Window;
 
 pub fn main() {
     read_addon_dir("./example-data");
-
-    // Creates the window.
-    let _window = Window::new((1050, 620));
+    gui::run();
 }
