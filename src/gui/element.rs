f0de9e0f3cdc5c65e83b7658a327af0a1306df17
Casper Storm <casper+github@rogildstorm.com>
2020-08-26 21:33:33 +0200

    fix: extracted variables into statics

--- src/gui/element.rs
+++ src/gui/element.rs
@@ -6,18 +6,19 @@ use {
     },
 };
 
+// Default values used on multiple elements.
+static DEFAULT_FONT_SIZE: u16 = 14;
+static DEFAULT_PADDING: u16 = 10;
+
 pub fn settings_container<'a>(
     wow_path_button_state: &'a mut button::State,
     config: &Config,
 ) -> Container<'a, Message> {
-    let default_font_size = 14;
-    let default_padding = 10;
-
     // Title row
     let wow_directory_info_text = Text::new("World of Warcraft directory").size(14);
     let wow_title_row = Row::new()
         .push(wow_directory_info_text)
-        .padding(default_padding);
+        .padding(DEFAULT_PADDING);
 
     // Data row
     let path_str = config
@@ -28,14 +29,14 @@ pub fn settings_container<'a>(
         .unwrap_or("No path set.");
     let open_directory: Element<Interaction> = Button::new(
         wow_path_button_state,
-        Text::new("Select Directory").size(default_font_size),
+        Text::new("Select Directory").size(DEFAULT_FONT_SIZE),
     )
     .style(style::DefaultBoxedButton)
     .on_press(Interaction::OpenDirectory)
     .into();
 
     // We add some margin left to adjust for inner-marigin in cell.
-    let left_spacer = Space::new(Length::Units(default_padding), Length::Units(0));
+    let left_spacer = Space::new(Length::Units(DEFAULT_PADDING), Length::Units(0));
 
     let wow_directory_data_text = Text::new(path_str)
         .size(14)
@@ -51,7 +52,7 @@ pub fn settings_container<'a>(
         .push(open_directory.map(Message::Interaction))
         .push(wow_directory_data_text_container);
 
-    let bottom_space = Space::new(Length::FillPortion(1), Length::Units(default_padding));
+    let bottom_space = Space::new(Length::FillPortion(1), Length::Units(DEFAULT_PADDING));
     let column = Column::new()
         .push(wow_title_row)
         .push(path_data_row)
@@ -65,8 +66,6 @@ pub fn addon_data_cell<'a>(
     addon: &'a mut Addon,
     is_addon_expanded: bool,
 ) -> Container<'a, Message> {
-    let default_font_size = 14;
-    let default_padding = 10;
     let default_height = Length::Units(26);
 
     // Check if current addon is expanded.
@@ -76,7 +75,7 @@ pub fn addon_data_cell<'a>(
         .clone()
         .unwrap_or_else(|| String::from("-"));
 
-    let title = Text::new(&addon.title).size(default_font_size);
+    let title = Text::new(&addon.title).size(DEFAULT_FONT_SIZE);
     let title_container = Container::new(title)
         .height(default_height)
         .width(Length::FillPortion(1))
@@ -84,7 +83,7 @@ pub fn addon_data_cell<'a>(
         .padding(5)
         .style(style::AddonRowDefaultTextContainer);
 
-    let installed_version = Text::new(version).size(default_font_size);
+    let installed_version = Text::new(version).size(DEFAULT_FONT_SIZE);
     let installed_version_container = Container::new(installed_version)
         .height(default_height)
         .width(Length::Units(150))
@@ -92,7 +91,7 @@ pub fn addon_data_cell<'a>(
         .padding(5)
         .style(style::AddonRowSecondaryTextContainer);
 
-    let remote_version = Text::new(remote_version).size(default_font_size);
+    let remote_version = Text::new(remote_version).size(DEFAULT_FONT_SIZE);
     let remote_version_container = Container::new(remote_version)
         .height(default_height)
         .width(Length::Units(150))
@@ -103,7 +102,7 @@ pub fn addon_data_cell<'a>(
     let update_button_width = Length::Units(85);
     let update_button_container = match &addon.state {
         AddonState::Ajour(string) => Container::new(
-            Text::new(string.clone().unwrap_or_else(|| "".to_string())).size(default_font_size),
+            Text::new(string.clone().unwrap_or_else(|| "".to_string())).size(DEFAULT_FONT_SIZE),
         )
         .height(default_height)
         .width(update_button_width)
@@ -116,7 +115,7 @@ pub fn addon_data_cell<'a>(
                 &mut addon.update_btn_state,
                 Text::new("Update")
                     .horizontal_alignment(HorizontalAlignment::Center)
-                    .size(default_font_size),
+                    .size(DEFAULT_FONT_SIZE),
             )
             .style(style::SecondaryButton)
             .on_press(Interaction::Update(id))
@@ -129,21 +128,21 @@ pub fn addon_data_cell<'a>(
                 .center_x()
                 .style(style::AddonRowDefaultTextContainer)
         }
-        AddonState::Downloading => Container::new(Text::new("Downloading").size(default_font_size))
+        AddonState::Downloading => Container::new(Text::new("Downloading").size(DEFAULT_FONT_SIZE))
             .height(default_height)
             .width(update_button_width)
             .center_y()
             .center_x()
             .padding(5)
             .style(style::AddonRowSecondaryTextContainer),
-        AddonState::Unpacking => Container::new(Text::new("Unpacking").size(default_font_size))
+        AddonState::Unpacking => Container::new(Text::new("Unpacking").size(DEFAULT_FONT_SIZE))
             .height(default_height)
             .width(update_button_width)
             .center_y()
             .center_x()
             .padding(5)
             .style(style::AddonRowSecondaryTextContainer),
-        AddonState::Loading => Container::new(Text::new("Loading").size(default_font_size))
+        AddonState::Loading => Container::new(Text::new("Loading").size(DEFAULT_FONT_SIZE))
             .height(default_height)
             .width(update_button_width)
             .center_y()
@@ -159,7 +158,7 @@ pub fn addon_data_cell<'a>(
 
     let details_button: Element<Interaction> = Button::new(
         &mut addon.details_btn_state,
-        Text::new(details_button_text).size(default_font_size),
+        Text::new(details_button_text).size(DEFAULT_FONT_SIZE),
     )
     .on_press(Interaction::Expand(addon.id.clone()))
     .style(style::DefaultButton)
@@ -172,8 +171,8 @@ pub fn addon_data_cell<'a>(
         .center_x()
         .style(style::AddonRowSecondaryTextContainer);
 
-    let left_spacer = Space::new(Length::Units(default_padding), Length::Units(0));
-    let right_spacer = Space::new(Length::Units(default_padding + 5), Length::Units(0));
+    let left_spacer = Space::new(Length::Units(DEFAULT_PADDING), Length::Units(0));
+    let right_spacer = Space::new(Length::Units(DEFAULT_PADDING + 5), Length::Units(0));
 
     let row = Row::new()
         .push(left_spacer)
@@ -192,15 +191,15 @@ pub fn addon_data_cell<'a>(
             .notes
             .clone()
             .unwrap_or_else(|| "No description for addon.".to_string());
-        let left_spacer = Space::new(Length::Units(default_padding), Length::Units(0));
-        let right_spacer = Space::new(Length::Units(default_padding + 5), Length::Units(0));
-        let space = Space::new(Length::Units(0), Length::Units(default_padding * 2));
+        let left_spacer = Space::new(Length::Units(DEFAULT_PADDING), Length::Units(0));
+        let right_spacer = Space::new(Length::Units(DEFAULT_PADDING + 5), Length::Units(0));
+        let space = Space::new(Length::Units(0), Length::Units(DEFAULT_PADDING * 2));
         let bottom_space = Space::new(Length::Units(0), Length::Units(4));
-        let notes_text = Text::new(notes).size(default_font_size);
+        let notes_text = Text::new(notes).size(DEFAULT_FONT_SIZE);
 
         let mut force_download_button = Button::new(
             &mut addon.force_btn_state,
-            Text::new("Force update").size(default_font_size),
+            Text::new("Force update").size(DEFAULT_FONT_SIZE),
         )
         .style(style::DefaultBoxedButton);
 
@@ -217,7 +216,7 @@ pub fn addon_data_cell<'a>(
 
         let delete_button: Element<Interaction> = Button::new(
             &mut addon.delete_btn_state,
-            Text::new("Delete").size(default_font_size),
+            Text::new("Delete").size(DEFAULT_FONT_SIZE),
         )
         .on_press(Interaction::Delete(addon.id.clone()))
         .style(style::DeleteBoxedButton)
@@ -254,36 +253,34 @@ pub fn addon_data_cell<'a>(
 }
 
 pub fn addon_row_titles<'a>(addons: &[Addon]) -> Row<'a, Message> {
-    let default_font_size = 14;
-    let default_padding = 10;
     // A row containing titles above the addon rows.
     let mut row_titles = Row::new().spacing(1).height(Length::Units(20));
 
     // We add some margin left to adjust for inner-marigin in cell.
-    let left_spacer = Space::new(Length::Units(default_padding + 5), Length::Units(0));
-    let right_spacer = Space::new(Length::Units(default_padding), Length::Units(0));
+    let left_spacer = Space::new(Length::Units(DEFAULT_PADDING + 5), Length::Units(0));
+    let right_spacer = Space::new(Length::Units(DEFAULT_PADDING), Length::Units(0));
 
-    let addon_row_text = Text::new("Addon").size(default_font_size);
+    let addon_row_text = Text::new("Addon").size(DEFAULT_FONT_SIZE);
     let addon_row_container = Container::new(addon_row_text)
         .width(Length::FillPortion(1))
         .style(style::SecondaryTextContainer);
 
-    let local_version_text = Text::new("Local").size(default_font_size);
+    let local_version_text = Text::new("Local").size(DEFAULT_FONT_SIZE);
     let local_version_container = Container::new(local_version_text)
         .width(Length::Units(150))
         .style(style::SecondaryTextContainer);
 
-    let remote_version_text = Text::new("Remote").size(default_font_size);
+    let remote_version_text = Text::new("Remote").size(DEFAULT_FONT_SIZE);
     let remote_version_container = Container::new(remote_version_text)
         .width(Length::Units(150))
         .style(style::SecondaryTextContainer);
 
-    let status_row_text = Text::new("Status").size(default_font_size);
+    let status_row_text = Text::new("Status").size(DEFAULT_FONT_SIZE);
     let status_row_container = Container::new(status_row_text)
         .width(Length::Units(85))
         .style(style::SecondaryTextContainer);
 
-    let delete_row_text = Text::new("Details").size(default_font_size);
+    let delete_row_text = Text::new("Details").size(DEFAULT_FONT_SIZE);
     let delete_row_container = Container::new(delete_row_text)
         .width(Length::Units(70))
         .style(style::SecondaryTextContainer);
@@ -311,22 +308,18 @@ pub fn menu_container<'a>(
     addons: &[Addon],
     config: &Config,
 ) -> Container<'a, Message> {
-    // FIXME: Maybe we should have these as statics?
-    let default_font_size = 14;
-    let default_padding = 10;
-
     // A row contain general settings.
     let mut settings_row = Row::new().spacing(1).height(Length::Units(35));
 
     let mut update_all_button = Button::new(
         update_all_button_state,
-        Text::new("Update All").size(default_font_size),
+        Text::new("Update All").size(DEFAULT_FONT_SIZE),
     )
     .style(style::DefaultBoxedButton);
 
     let mut refresh_button = Button::new(
         refresh_button_state,
-        Text::new("Refresh").size(default_font_size),
+        Text::new("Refresh").size(DEFAULT_FONT_SIZE),
     )
     .style(style::DefaultBoxedButton);
 
@@ -350,9 +343,9 @@ pub fn menu_container<'a>(
         .filter(|a| a.state == AddonState::Loading)
         .count();
     let status_text = if loading_addons != 0 {
-        Text::new(format!("Fetching data for {} addons", loading_addons)).size(default_font_size)
+        Text::new(format!("Fetching data for {} addons", loading_addons)).size(DEFAULT_FONT_SIZE)
     } else {
-        Text::new(format!("{} addons loaded", parent_addons_count)).size(default_font_size)
+        Text::new(format!("{} addons loaded", parent_addons_count)).size(DEFAULT_FONT_SIZE)
     };
 
     let status_container = Container::new(status_text)
@@ -362,7 +355,7 @@ pub fn menu_container<'a>(
 
     // Displays an error, if any has occured.
     let error_text = if let AjourState::Error(e) = state {
-        Text::new(e.to_string()).size(default_font_size)
+        Text::new(e.to_string()).size(DEFAULT_FONT_SIZE)
     } else {
         // Display nothing.
         Text::new("")
@@ -376,7 +369,7 @@ pub fn menu_container<'a>(
         .style(style::StatusErrorTextContainer);
 
     let version_text = Text::new(env!("CARGO_PKG_VERSION"))
-        .size(default_font_size)
+        .size(DEFAULT_FONT_SIZE)
         .horizontal_alignment(HorizontalAlignment::Right);
     let version_container = Container::new(version_text)
         .center_y()
@@ -387,7 +380,7 @@ pub fn menu_container<'a>(
         settings_button_state,
         Text::new("Settings")
             .horizontal_alignment(HorizontalAlignment::Center)
-            .size(default_font_size),
+            .size(DEFAULT_FONT_SIZE),
     )
     .style(style::DefaultBoxedButton)
     .on_press(Interaction::Settings)
@@ -396,8 +389,8 @@ pub fn menu_container<'a>(
     let spacer = Space::new(Length::Units(7), Length::Units(0));
     // Not using default padding, just to make it look prettier UI wise
     let top_spacer = Space::new(Length::Units(0), Length::Units(5));
-    let left_spacer = Space::new(Length::Units(default_padding), Length::Units(0));
-    let right_spacer = Space::new(Length::Units(default_padding + 5), Length::Units(0));
+    let left_spacer = Space::new(Length::Units(DEFAULT_PADDING), Length::Units(0));
+    let right_spacer = Space::new(Length::Units(DEFAULT_PADDING + 5), Length::Units(0));
 
     // Surrounds the elements with spacers, in order to make the GUI look good.
     settings_row = settings_row
