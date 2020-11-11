3045c992916f316d93e09e957252399557b9a3f4
Casper Rogild Storm <casper+github@rogildstorm.com>
2020-11-11 17:59:32 +0100

    chore: larger buttons in settings.

--- src/gui/element.rs
+++ src/gui/element.rs
@@ -57,7 +57,7 @@ pub fn settings_container<'a, 'b>(
 
     let directory_button: Element<Interaction> =
         Button::new(directory_button_state, directory_button_title_container)
-            .width(Length::Units(100))
+            .width(Length::Units(120))
             .style(style::DefaultBoxedButton(color_palette))
             .on_press(Interaction::OpenDirectory(DirectoryType::Wow))
             .into();
@@ -101,7 +101,7 @@ pub fn settings_container<'a, 'b>(
         Message::ThemeSelected,
     )
     .text_size(14)
-    .width(Length::Units(100))
+    .width(Length::Units(120))
     .style(style::PickList(color_palette));
 
     // Data row for theme picker list.
@@ -185,7 +185,7 @@ pub fn settings_container<'a, 'b>(
             &mut backup_state.directory_btn_state,
             directory_button_title_container,
         )
-        .width(Length::Units(100))
+        .width(Length::Units(120))
         .style(style::DefaultBoxedButton(color_palette))
         .on_press(Interaction::OpenDirectory(DirectoryType::Backup))
         .into();
@@ -233,7 +233,7 @@ pub fn settings_container<'a, 'b>(
                 &mut backup_state.backup_now_btn_state,
                 backup_button_title_container,
             )
-            .width(Length::Units(100))
+            .width(Length::Units(120))
             .style(style::DefaultBoxedButton(color_palette));
 
             // Only show button as clickable if it's not currently backing up and
