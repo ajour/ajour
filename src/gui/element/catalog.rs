0fd099cc81a77797e6cb6aa5d37cb0813433ab0d
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-02-09 08:58:35 +0100

    Update catalog.rs

--- src/gui/element/catalog.rs
+++ src/gui/element/catalog.rs
@@ -6,7 +6,8 @@ use {
     },
     crate::localization::{localized_string, localized_timeago_formatter},
     ajour_core::{
-        config::Config, theme::ColorPalette, utility::format_interface_into_game_version,
+        catalog::Source, config::Config, theme::ColorPalette,
+        utility::format_interface_into_game_version,
     },
     ajour_widgets::{header, Header, TableRow},
     chrono::prelude::*,
@@ -278,7 +279,10 @@ pub fn data_row_container<'a, 'b>(
             .game_versions
             .iter()
             .find(|gv| gv.flavor == config.wow.flavor.base_flavor())
-            .map(|gv| format_interface_into_game_version(&gv.game_version[..]))
+            .map(|gv| match addon_data.source {
+                Source::TownlongYak => format_interface_into_game_version(&gv.game_version[..]),
+                _ => gv.game_version.clone(),
+            })
             .unwrap_or_else(|| "-".to_owned());
 
         let game_version_text = Text::new(game_version_text).size(DEFAULT_FONT_SIZE);
