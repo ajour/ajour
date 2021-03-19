bbd283870a129a3e038fab455de361a5dd369770
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-03-19 08:39:00 +0100

    fix: creating default ajour config dir

--- crates/core/src/fs/mod.rs
+++ crates/core/src/fs/mod.rs
@@ -25,13 +25,7 @@ pub static CONFIG_DIR: Lazy<Mutex<PathBuf>> = Lazy::new(|| {
     #[cfg(not(windows))]
     {
         let home = env::var("HOME").expect("user home directory not found.");
-
         let config_dir = PathBuf::from(&home).join(".config/ajour");
-
-        if !config_dir.exists() {
-            fs::create_dir_all(&config_dir).expect("could not create folder $HOME/.config/ajour");
-            log::debug!("config directory created");
-        }
 
         Mutex::new(config_dir)
     }
@@ -45,11 +39,6 @@ pub static CONFIG_DIR: Lazy<Mutex<PathBuf>> = Lazy::new(|| {
         let config_dir = dirs_next::config_dir()
             .map(|path| path.join("ajour"))
             .expect("user home directory not found.");
-
-        if !config_dir.exists() {
-            fs::create_dir(&config_dir).expect("could not create folder %APPDATA%\\ajour");
-            log::debug!("config directory created");
-        }
 
         Mutex::new(config_dir)
     }
