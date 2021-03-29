b5961a789500160a97e8a05fe800c3a91a8ed1c5
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-03-29 21:23:30 +0200

    chore: save to config if its a single account.

--- src/gui/update.rs
+++ src/gui/update.rs
@@ -2091,14 +2091,16 @@ pub fn handle_message(ajour: &mut Ajour, message: Message) -> Result<Command<Mes
                     _ => None,
                 };
 
-                if let Some(account) = account_from_config.or_else(get_single_account) {
-                    // Ensure config is updated.
+                // If there is only a single account, we save it to the config.
+                if let Some(single_account) = get_single_account() {
                     ajour
                         .config
                         .weak_auras_account
-                        .insert(flavor, account.clone());
+                        .insert(flavor, single_account);
                     let _ = ajour.config.save();
+                }
 
+                if let Some(account) = account_from_config.or_else(get_single_account) {
                     if let Some(wtf_path) = ajour.config.get_wtf_directory_for_flavor(&flavor) {
                         state.chosen_account = Some(account.clone());
 
