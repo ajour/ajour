874d28608218933a3c74ecfc64398646586444df
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-03-30 12:10:33 +0200

    chore: updated fn names and documentation

--- src/command/update_weakauras.rs
+++ src/command/update_weakauras.rs
@@ -6,8 +6,8 @@ use ajour_weak_auras::{get_aura_updates, parse_auras, write_updates};
 use anyhow::{bail, Context};
 use async_std::task;
 
-pub fn update_all_weakauras() -> Result<()> {
-    log::info!("Checking for WeakAura updates...");
+pub fn update_all_auras() -> Result<()> {
+    log::info!("Checking for WeakAura and Plater updates...");
 
     task::block_on(async {
         let config = load_config().await?;
@@ -19,7 +19,11 @@ pub fn update_all_weakauras() -> Result<()> {
             if let Some(account) = config.weak_auras_account.get(flavor).cloned() {
                 flavors_setup += 1;
 
-                log::info!("{} - Parsing WeakAuras for account {}", flavor, &account);
+                log::info!(
+                    "{} - Parsing WeakAura and Plater for account {}",
+                    flavor,
+                    &account
+                );
 
                 let wtf_path = match config.get_wtf_directory_for_flavor(flavor) {
                     Some(path) => path,
@@ -69,8 +73,9 @@ pub fn update_all_weakauras() -> Result<()> {
                 for slug in updated_slugs.iter() {
                     if let Some(aura) = auras.iter().find(|a| a.slug() == slug) {
                         log::info!(
-                            "{} - \t{} - {} -> {}",
+                            "{} - \t({}) {} ({} -> {})",
                             flavor,
+                            aura.kind(),
                             aura.name(),
                             aura.installed_symver().unwrap_or_default(),
                             aura.remote_symver()
@@ -82,7 +87,7 @@ pub fn update_all_weakauras() -> Result<()> {
 
         if flavors_setup == 0 {
             bail!(
-                "No accounts have been specified for WeakAuras. Launch Ajour and make sure to specify an account for each flavor you want WeakAuras updates for."
+                "No accounts have been specified for WeakAura and Plater. Launch Ajour and make sure to specify an account for each flavor you want updates for."
             );
         }
 
